7359c2d17d211e8631e22b8fa329ced76112026e
milchevdimitar <85071480+milchevdimitar@users.noreply.github.com>
2022-01-03 03:25:35 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+#Copiright Dimitar Milchev
+#не е тествано под debian, защото ползвам arch :)
+
+dpkg -s $1 &> /dev/null
+$1 = 'python3-tk'
+
+if [ $? -eq 0 ]; then
+    python main.py
+else
+    sudo apt install python3-tk
+fi
